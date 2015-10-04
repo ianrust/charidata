bf27796b41309c067bc516c5d9885f87b72c2132
Ian Rust <ian@getcruise.com>
2015-10-04 11:33:10 -0700

    added api endpoint for haiti

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/haitilog', function(request, response) {
+  console.log(request);
+  res.sendStatus(200);
+});
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
@@ -16,5 +21,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
